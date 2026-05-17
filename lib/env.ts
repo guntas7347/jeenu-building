8d28a6e5364646a1a9f566ab4005d3b0a281c577
Guntas Singh <guntas7347@gmail.com>
2026-05-17 21:15:45 +0530

    temp fix

--- lib/env.ts
+++ lib/env.ts
@@ -2,15 +2,13 @@ import "dotenv/config";
 
 function requireEnv(name: string): string {
   const value = process.env[name];
+  // if (!value) {
+  //   throw new Error(`Missing required environment variable: ${name}`);
+  // }
 
-  if (value === undefined || value.trim() === "") {
-    throw new Error(`Missing required environment variable: ${name}`);
-  }
-
-  return value;
+  return value as string;
 }
 const env = {
-  NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL: requireEnv("DATABASE_URL"),
   NEXTAUTH_SECRET: requireEnv("NEXTAUTH_SECRET"),
   // NEXTAUTH_URL: requireEnv("NEXTAUTH_URL"),
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,11 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../prisma/generated/client";
-import env from "./env";
 
-const pool = new PrismaPg({ connectionString: env.DATABASE_URL });
+const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter: pool });
 
 const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
